a0c50fd67b02637b6b3818519a49b3aefc58c31d
gorilla-Kim <00ghks22@naver.com>
2019-12-15 23:30:53 +0900

    handleListening 서버 시작시 콘솔에 알림 메시지 출력 함수

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // require : 해당 모듈을 어디선가에서 가져오는 겁니다.
 const express = require('express')
 const app = express()
+const PORT = 4000; 
 
-app.listen(4000);
+
+const handleListening = () => {
+    console.log(`Listening on : http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
