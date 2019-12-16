dc708375d399fc05cc06050ee98926703243db2c
gorilla-Kim <00ghks22@naver.com>
2019-12-16 17:09:45 +0900

    update

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000; 
@@ -22,6 +24,9 @@ const handleProfile = (req, res) => {
     res.send("You are my profile");
 }
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
