a34050da543dbca572b7ab4f7270bafff50e738e
gorilla-Kim <00ghks22@naver.com>
2019-12-16 16:06:17 +0900

    middleware 개별로 적용하는 방법과 전역으로 사용하는 방법을 익힘 전역 = app.use 개별은 router마다 중간함수를 추가하여 사용

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # wetube-nodeJs
 
-## 1Day
+## 1 Day
+
+### 📒 Express
+
+* 외부 모듈 가져오기(express 가져오기)
+
+```javascript
+// require : 해당 모듈을 어디선가에서 가져오는 겁니다.
+const express = require('express')
+// 실행한 express 앱 만들기
+const app = express()
+```
+
+* request, response 다루는 함수 만들기
+
+```javascript
+const handleHome = (req, res) => {
+    // console.log(req);
+    res.send("welcome to the home!!")
+}
+
+const handleProfile = (req, res) => {
+    res.send("You are my profile");
+}
+```
+
+* listener 만들기
+
+```javascript
+app.listen(PORT, handleListening);
+```
+
+* router 만들기
+
+```javascript
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+```
+
--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 // require : 해당 모듈을 어디선가에서 가져오는 겁니다.
-const express = require('express')
-const app = express()
+// const express = require('express')
+import express from "express";
+const app = express();
 const PORT = 4000; 
 
 
 const handleListening = () => {
     console.log(`Listening on : http://localhost:${PORT}`);
 }
+
+const handleHome = (req, res) => {
+    // console.log(req);
+    res.send("welcome to the home!!")
+}
+
+const betweenHome = (req, res, next) => {
+    console.log("middle ware test!!");
+    next();
+}
+
+const handleProfile = (req, res) => {
+    res.send("You are my profile");
+}
+
+// app.use 는 작성하는 위치가 중요하다.
+app.use(betweenHome);
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
 
 app.listen(PORT, handleListening);
