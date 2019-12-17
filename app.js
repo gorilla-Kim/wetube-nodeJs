57f3601b3a185efad500a15732f9b5a34cdd22c7
gorilla-Kim <00ghks22@naver.com>
2019-12-17 11:26:32 +0900

    init.js/ app.js app.js 에는 필요한 기능만 추가 init.js 에는 서버 실행관련 기능만 추가

--- app.js
+++ app.js
@@ -7,13 +7,7 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 
 const app = express();
-const PORT = 4000; 
 
-
-
-const handleListening = () => {
-    console.log(`Listening on : http://localhost:${PORT}`);
-}
 
 const handleHome = (req, res) => {
     // console.log(req);
@@ -23,15 +17,19 @@ const handleHome = (req, res) => {
 const handleProfile = (req, res) => {
     res.send("You are my profile");
 }
-
+/**********************************************/
+/*                   미들웨어                 */
+/*********************************************/
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
+/**********************************************/
+/*                   Router                  */
+/*********************************************/
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
-
-app.listen(PORT, handleListening);
+export default app;
