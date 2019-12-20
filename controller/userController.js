af3d93aa70ae200627eaeb97fad1e34021f44d56
gorilla-Kim <00ghks22@naver.com>
2019-12-20 22:23:37 +0900

    Login url처리 로그인 GET 과 POST에 대한 경로처리

--- controller/userController.js
+++ controller/userController.js
@@ -17,7 +17,16 @@ export const postJoin = (req, res) => {
     }
 }
 
-export const login = (req, res) => res.render("login", { pageTitle: "Login"});
+export const getLogin = (req, res) => {
+    res.render("login", { pageTitle: "Login"});
+}
+export const postLogin = (req, res) => {
+    const {
+        body: {email, password}
+    } = req;
+
+    res.redirect(routes.home);
+}
 export const logout = (req, res) => res.render("logout", { pageTitle: "Logout"});
 
 export const users = (req, res) => res.render("users", { pageTitle: "Users"});
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,16 +1,18 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controller/videoController";
-import { getJoin, postJoin, login, logout } from "../controller/userController";
+import { getJoin, postJoin, getLogin, postLogin, logout } from "../controller/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
