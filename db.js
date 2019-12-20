2a384e1e99ea9934e69d3b13f5af55bdf534ac34
gorilla-Kim <00ghks22@naver.com>
2019-12-20 17:12:50 +0900

    데이터베이스 사용 가짜 데이터베이스를 만들고 가짜 데이터를 사용

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id: 324393,
+        title: '1test video',
+        description: '1this is test video',
+        views: 24,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 121212,
+            name: "1Kim-Gorilla",
+            email: "1gorilla@gmail.com"
+        }       
+    },
+    {
+        id: 324394,
+        title: '2test video',
+        description: '2this is test video',
+        views: 24,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 121213,
+            name: "2Kim-Gorilla",
+            email: "2gorilla@gmail.com"
+        }       
+    },
+    {
+        id: 324395,
+        title: '3test video',
+        description: '3this is test video',
+        views: 24,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 121214,
+            name: "3Kim-Gorilla",
+            email: "3gorilla@gmail.com"
+        }       
+    }
+]
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,9 +4,9 @@ import { users, userDetail, editProfile, changePassword } from "../controller/us
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
+// userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
