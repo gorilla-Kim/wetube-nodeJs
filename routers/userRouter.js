907afb4f1e6d0daa86506f4dcaf9f7834e85fafa
gorilla-Kim <00ghks22@naver.com>
2019-12-20 22:40:27 +0900

    인증과 특정 데이터접근 가짜 데이터를 통한 인증구축 특정 유저나 비디오에 대한 상세보기 url 처리

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -7,6 +7,6 @@ const userRouter = express.Router();
 // userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
