d5bd1d62b6d3cf2d1f6bcd5b787e8135137366ac
gorilla-Kim <00ghks22@naver.com>
2019-12-21 23:43:40 +0900

    Security dotenv 설치 .dot 파일 생성 .dot 파일안에 숨기고자 하는 키값들을 넣어준다(ex. port, url and so on...) 이렇게 함으로서 다른 유저들이 port번호와 같은것을 웹파일 문서에서 영탐하는것을 방지할 수 있다.

--- init.js
+++ init.js
@@ -1,6 +1,10 @@
 import app from "./app";
+import dotenv from "dotenv";
+import "./db";
 
-const PORT = 4000;
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`✅  Listening on : http://localhost:${PORT}`);
 
