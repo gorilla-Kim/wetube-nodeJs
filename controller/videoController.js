59251c1b217e868c77fb2c8be0618bfc4531c20a
gorilla-Kim <00ghks22@naver.com>
2019-12-23 14:11:16 +0900

    Video upload & Multer 비디오를 업로드할때 도와주는 middlware = multer multer를 이용해서 video의 저장 url을 가져올 수 있다. 이를 디비에 저장해서 사용한다. 절대 디비에 영상자체를 저장하지않는다

--- controller/videoController.js
+++ controller/videoController.js
@@ -19,12 +19,19 @@ export const search = (req, res) =>{
 export const getUpload = (req, res) => {
     res.render("upload", { pageTitle: "Upload" });
 }
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
     const {
-        body: {file, title, description}
+        body: {title, description},
+        file: {path}
     } = req;
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    });
+    console.log(newVideo);
     // 할일 : video 업로드 및 저장
-    res.redirect(routes.videoDetail(324393))
+    res.redirect(routes.videoDetail(newVideo.id))
 }
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from "../routes";
 import { videos, getUpload, postUpload, videoDetail, editVideo, deleteVideo } from "../controller/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
 // videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
