6adece374a89403ec938326a7e6df3095572be70
gorilla-Kim <00ghks22@naver.com>
2019-12-26 18:54:53 +0900

    게시글 sorting 최신 게시글이 위로 올라가게 setting

--- controller/videoController.js
+++ controller/videoController.js
@@ -4,7 +4,7 @@ import Video from "../models/Video";
 export const home = async (req, res) => {
     try {
         // 장고의 Object.all() 과 같음
-        const videos = await Video.find({});
+        const videos = await Video.find({}).sort({_id: -1});
         res.render("home", { pageTitle: "Home", videos });
     } catch (error) {
         console.log(error);
