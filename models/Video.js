1fa1f01d8872d33653c7474d5c35df82b94d8068
gorilla-Kim <00ghks22@naver.com>
2019-12-26 20:14:13 +0900

    video detail error solved! 해결!
    Model 에서 Video.js에서 오타가 있었다.... ㅎㅎ

--- models/Video.js
+++ models/Video.js
@@ -18,7 +18,7 @@ const VideoSchema = new mongoose.Schema({
         type : Date,
         default : Date.now
     },
-    comment : [
+    comments : [
         {
         type : mongoose.Schema.Types.ObjectId,
         ref : "Comment"
