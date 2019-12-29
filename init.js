30998713006b8d4e65e9f0714e50731bc990a6fe
gorilla-Kim <00ghks22@naver.com>
2019-12-29 23:44:01 +0900

    인증(Passport) Passport 는 세션을 이용한 인증을 구현해주는 미들웨어 이다. 소셜 로그인도 가능하다.
    User.js 는 Usert DB를 생성하기 위한 모델이다.

--- init.js
+++ init.js
@@ -4,6 +4,10 @@ import "./db";
 
 dotenv.config();
 
+import "./models/Video";
+import "./models/Comment";
+import "./models/User";
+
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`✅  Listening on : http://localhost:${PORT}`);
